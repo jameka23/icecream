df0348665f7d2ce694878e1d506fb99bf695137e
Jameka Echols <jameka.echols@gmail.com>
2025-11-09 17:40:59 -0500

    Initial Commit

---
+++ icecreammania/icecreammaniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  icecreammaniaApp.swift
+//  icecreammania
+//
+//  Created by Jameka Echols on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct icecreammaniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ icecreammaniaTests/icecreammaniaTests.swift
@@ -0,0 +1,17 @@
+//
+//  icecreammaniaTests.swift
+//  icecreammaniaTests
+//
+//  Created by Jameka Echols on 11/9/25.
+//
+
+import Testing
+@testable import icecreammania
+
+struct icecreammaniaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
